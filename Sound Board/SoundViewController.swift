68d6ccd4a4307429660d2dbc0c12db5f93db278b
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-10 19:49:32 +0800

    Initial commit

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by John Crisostomo on 10/04/2017.
+//  Copyright © 2017 John Crisostomo. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+        
+    }
+}
